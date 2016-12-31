9b8840fb0d05598e1d35083ad0397c1fe15d94a9
dylanfisher <hi@dylanfisher.com>
2016-12-30 22:34:57 -0500

    Remove extra break tags

--- colophon.php
+++ colophon.php
@@ -5,11 +5,11 @@
     <div class="col-sm-10 col-sm-offset-1 ta-center">
       <p>
         <span class="title">DESIGN &amp; ILLUSTRATION</span><br>
-        <a href="http://nikasimovich.com/">Nika Simovich</a>
+        <a href="http://nikasimovich.com/" target="_blank">Nika Simovich</a>
       </p>
       <p>
         <span class="title">PROGRAMMING</span><br>
-        <a href="http://dylanfisher.com/">Dylan Fisher</a>
+        <a href="http://dylanfisher.com/" target="_blank">Dylan Fisher</a>
       </p>
       <p>
         <span class="title">TYPOGRAPHY</span><br>
--- purchase.php
+++ purchase.php
@@ -4,10 +4,9 @@
   <div class="row">
     <div class="col-sm-10 col-sm-offset-1 ta-center">
       <p class="title">THE ORIGINAL PRINTED SERIES</p>
-      <br>
-      <img src="images/ui/wildflowers-set.png" alt="Wildflowers printed set">
-      <br>
-      <br>
+      <p>
+        <img src="images/ui/wildflowers-set.png" alt="Wildflowers printed set">
+      </p>
       <p>
         To purchase, <a href="mailto:nika@nikasimovich.com">please get in touch</a>.
       </p>
